367cffd57f1431c80e8b7c9a24ba02903170b273
zhaozihu <2276443321@qq.com>
2019-05-17 18:27:16 +0800

    精准推广项目

--- buzz/jobbole_spider.py
+++ buzz/jobbole_spider.py
@@ -1,37 +1,47 @@
 #coding=gbk
 import time
-from tornado import ioloop,httpclient, gen 
-import requests
+from tornado import ioloop, httpclient, gen
 from lxml import etree
+import requests
 
-Start = 'http://python.jobbole.com/all-posts/'
+Start = 'https://s.hc360.com/company/search.html?kwd=塑业'
 
 count = 0 #统计爬取了几个文章
 #----------------------------------------------------------------------
-def get_archive(node):
-    """从a标签中获取文章链接"""
-    title = node.text
-    url = node.attrib['href']
-    return title,url
-    
-async def fetch_list(url=Start):
-    global count
-    #获取页面内容
+async def parse_archive(url):
+    # 获取页面内容
+    # resp = requests.get(url)
+    # html = resp.text
     resp = await httpclient.AsyncHTTPClient().fetch(url)
     html = resp.body.decode()
     selector = etree.HTML(html)
-    nodes = selector.xpath('//div[@class="post-meta"]//a[@class="archive-title"]')
-    for node in nodes:
-        print(get_archive(node))
+    CompanyName = selector.xpath('//a[@id="companyName"]')
+    CompanyNum = selector.xpath('//div[@class="leftBoxCon ContactBox"]/ul/li[3]')
+    return CompanyName,CompanyNum
+
+    
+async def fetch_list(url=Start):
+    global count
+    # 获取页面内容
+    resp = await httpclient.AsyncHTTPClient().fetch(url)
+    html = resp.body.decode()
+    selector = etree.HTML(html)
+    # nodes = selector.xpath('//div[@class="post-meta"]//a[@class="archive-title"]')
+    urls = selector.xpath('//dt[@class="til"]//h3/a[1]/@href')
+    for url in urls:
+        print(parse_archive(url))
         count += 1
         print(count)
-        
-    #处理下一页
-    if url ==Start:
-        max_num = selector.xpath('//div[contains(@class,"navigation")]/a[contains(@class,"page-numbers")]')[-2]
-        max_num = int(max_num.text)
-        tasks = [ fetch_list(f"http://python.jobbole.com/all-posts/page/{num}") for num in range(2,max_num+1)]
+
+    # 处理下一页
+    if url == Start:
+        # max_num = selector.xpath('//div[contains(@class,"navigation")]/a[contains(@class,"page-numbers")]')[-2]
+        # max_num = int(max_num.text)
+        # tasks = [fetch_list(f"http://python.jobbole.com/all-posts/page/{num}") for num in range(2, max_num+1)]
+        tasks = [fetch_list(f"https://s.hc360.com/company/search.html?kwd=塑业&pnum={num}") for num in range(2, 50)]
         await gen.multi(tasks)
+
+
 
 time1 = time.time()      
 loop = ioloop.IOLoop.current()
