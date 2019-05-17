3ed9dfbcc006d0993ff9d3bc48a332977525097a
zhaozihu <2276443321@qq.com>
2019-05-17 18:27:06 +0800

    精准推广项目

--- HuiCongWang_spider.py
+++ HuiCongWang_spider.py
@@ -7,25 +7,34 @@ import requests
 from lxml import etree
 
 def get_company_url(url):
-
-    rep = requests.get(url)
-    selector = etree.HTML(rep)
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 6.1; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3664.3 Safari/537.36'
+        }
+    response = requests.get(url,headers=headers)
+    if response.status_code ==200:
+        print("此页爬取成功")
+    html = response.text
+    selector = etree.HTML(html)
+    company_info = {}
     company_name = selector.xpath('//dt[@class="til"]///h3/a[1]/text()')
     company_url = selector.xpath('//dt[@class="til"]//h3/a[1]/@href')
-    return company_url
+    company_info[company_name] = company_url
+    return company_info
 
 
-def parse_company(url):
-# 提取公司联系人姓名和联系方式
+# def parse_company(url):
+# # 提取公司联系人姓名和联系方式
+#     respone = requests.get(url)
+#     rep = etree.HTML(respone)
+#     comapany_p_name = rep.xpath('//div[@class="leftBoxCon ContactBox"]/ul/li[1]')# 进一步选择
+#     comapany_p_num = rep.xpath('//div[@class="leftBoxCon ContactBox"]/ul/li[3]')# 进一步选择
+#     return comapany_p_name,comapany_p_num
 
-    comapany_p_name = rep.xpath('//div[@class="leftBoxCon ContactBox"]/ul/li[1]')# 进一步选择
-    comapany_p_num = rep.xpath('//div[@class="leftBoxCon ContactBox"]/ul/li[3]')# 进一步选择
 
-
-def next_page(url,page):
-    """获取下一页内容"""
-    next_url = url + "&pnum" + str(page)
-     return get_company_url(next_url)
+# def next_page(url,page):
+#     """获取下一页内容"""
+#     next_url = url + "&pnum" + str(page)
+#      return next
 
 
 if __name__ == '__main__':
@@ -34,8 +43,9 @@ if __name__ == '__main__':
     # print('请输入要查重的公司类型：（例如 塑业）')
     kw = "塑业"
     url = url + "/company/search.html?kwd=" + kw
-    get_company_url(url)
-    for page in range(2,50):
-        next_page(url,page)
+    company_info = get_company_url(url)
+    # for page in range(2,50):
+    #     next_page(url,page)
 
     print("爬取完成")
+    print(company_info)
